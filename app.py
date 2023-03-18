ac6ba0892372004d02814e3c122d5ded97313f42
Moyosore <msontop.me@gmail.com>
2023-03-19 00:03:56 +0100

    reset

--- app.py
+++ app.py
@@ -6,16 +6,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-<<<<<<< HEAD
-<<<<<<< HEAD
-def hello_world():
-    return 'Hello from Flask!'
-
-=======
 def index():
     return '<h1>Student Management API</h1>'
->>>>>>> 2d8621538a30d4b5e45c47ea4e687f38f2169fb5
-=======
-def index():
-    return '<h1>Student Management API</h1>'
->>>>>>> 2d8621538a30d4b5e45c47ea4e687f38f2169fb5
